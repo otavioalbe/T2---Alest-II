39fb92fe2b6693440875fd7fbb2764022c36c9c4
Otávio De Carli Albuquerque <126691564+otavioalbe@users.noreply.github.com>
2023-06-06 04:48:27 -0300

    Delete src directory

--- src/main/java/com/otavio/App.java
+++
@@ -1,9 +0,0 @@
-package com.otavio;
-
-import java.io.IOException;
-
-public class App{
-    public static void main(String[] args) throws IOException {
-        new Viagens().lerArquivo("mapa30.txt");;
-    }
-}
